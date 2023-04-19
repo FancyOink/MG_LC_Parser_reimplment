785e34c9232a8b3d435970a1ae0126848b80f18e
Johannes Kuhn <54399034+FancyOink@users.noreply.github.com>
2023-04-19 13:07:35 +0200

    added orcale and parser files.
    Added grammars as input to main.

---
+++ MG LC Parser v3/lcparser.pl
@@ -0,0 +1,4 @@
+% file: lcparser.pl
+% origin author : J. Kuhn
+% origin date: April 2023
+% purpose: takes a list of tokens and a lexicon and generates a fitting derivation tree
--- MG LC Parser v3/main.pl
+++ MG LC Parser v3/main.pl
@@ -1,8 +1,18 @@
 % file: main.pl
 % origin author : J. Kuhn
 % origin date: April 2023
-% purpose: top executable for MG LC Parser
+% purpose: top executable for MG LC-Parser
 
 :- ['helpers/painter'].
 :- ['scanner'].
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Verwendetes Lexikon für das Parsen
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:-['grammars/naus'].
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Hauptfunktion des Parsers
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+mgParse(Input,Lambda,Tree):- scan(Input,TokenList).
