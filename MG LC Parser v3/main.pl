4ef9e2b6d5b7848dccc6e5a75b618bf1265dd03b
Johannes Kuhn <54399034+FancyOink@users.noreply.github.com>
2023-06-26 09:19:13 +0200

    Added assertion of new Lis

--- MG LC Parser v3/main.pl
+++ MG LC Parser v3/main.pl
@@ -12,13 +12,43 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Verwendetes Lexikon für das Parsen
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%:-['grammars/maus'].
+:-['grammars/maus'].
 %:-['grammars/numbers'].
-:-['grammars/ZahlenSprache'].
+%:-['grammars/ZahlenSprache'].
 %:-['grammars/EpsKetten'].
 
-:-exterminate((Li,Eps)),write("Eta-Li: "),writeln(Li),length(Li,LLi),write("Eps-Li: "),writeln(Eps),length(Eps,LEps),write("#Eta-Li: "),write(LLi),write(" Eps-Li: "),writeln(LEps).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% rememberEta(+[Lis])
+%
+%	asserts Eta-Lis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rememberEta([]).
+rememberEta([Li|Lis]):- assertz(Li),rememberEta(Lis).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% rememberEps(+[Lis])
+%
+%	asserts Epsilon-Lis with the Mark = clean
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rememberEps([]).
+rememberEps([epsLi(FsE,clean)|Lis]):- assertz(epsLi(FsE,clean)),rememberEps(Lis).
+rememberEps([epsLi(_,dot)|Lis]):- rememberEps(Lis).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% exterminate(-([Eta-Li],[Epsilon-Li]))
+%
+% transforms the lexicon and removes epsilon-LI
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:-exterminate((Li,Eps)),
+	write("Eta-Li: "),writeln(Li),length(Li,LLi),write("Eps-Li: "),writeln(Eps),length(Eps,LEps),write("#Eta-Li: "),write(LLi),write(" Eps-Li: "),writeln(LEps),
+	rememberEta(Li),
+	rememberEps(Eps).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Hauptfunktion des Parsers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 mgParse(Input,Lambda,Tree):- scan(Input,TokenList).
+
+
+
